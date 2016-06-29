fbbc085d110f7fb69e17dcea909ca4a016e34c6f
ImplFancy <2045974872@qq.com>
2016-06-29 14:10:12 +0800

    Update Matrix3x2.h

--- utils/Matrix3x2.h
+++ utils/Matrix3x2.h
@@ -61,7 +61,31 @@ struct Matrix3x2
         _11 = _12 = _21 = _22 = _31 = _32 = (T)(0);
     }
 
+	std::array<T, 2> operator*(const std::array<T, 2> &orig) const {
+		std::array<T, 2> newVec;
+		newVec.at(0) = orig.at(0) * _11 + orig.at(1) * _21 + _31;
+		newVec.at(1) = orig.at(0) * _12 + orig.at(1) * _22 + _32;
+		return newVec;
+	}
 
+	Matrix3x2 CalcRevert() const {
+		if (Determinant() == 0) return *this;
+		Matrix3x2 result;
+		result._11 = _22 / Determinant();
+		result._22 = _11 / Determinant();
+		result._21 = _21 / Determinant();
+		result._31 = (_21 * _32 - _31*_22) / Determinant();
+		result._32 = -(_11*_32 - _31*_12) / Determinant();
+		//result._33 = (_11*_22 - _12*_21) / Determinant();
+		return result;
+	}
+
+
+private:
+	T Determinant() const {
+		return (_11 * _22 - _12 * _21);
+	}
+	
     T _11;
     T _12;
     T _21;
@@ -71,3 +95,14 @@ struct Matrix3x2
 };
 
 typedef Matrix3x2<float> Matrix3x2F;
+
+
+// std::array operator-
+template <class T>
+T scaleWith(const T& a1, const T& a2)
+{
+	T a;
+	for (typename T::size_type i = 0; i < a1.size(); i++)
+		a[i] = a1[i] * a2[i];
+	return a;
+}
